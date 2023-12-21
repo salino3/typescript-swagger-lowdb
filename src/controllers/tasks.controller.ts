bf14767d4ae16ab8ba34da9aecb8d5df8385f96d
salino3 <programacionflavio@gmail.com>
2023-12-21 15:26:30 +0100

    implement nanoid

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -1,8 +1,29 @@
 import { Handler, Request, Response } from "express";
+import {getConnection} from '../db';
 
 
-export const getTasks: Handler = async (req: Request, res: Response): Promise<void> => {
+export const getTasks: Handler = (req: Request, res: Response): void => {
 
- res.send("Good morning");
+ const data = getConnection().get('tasks').value();
+ res.json(data);
 
 };
+
+
+export const createTask: Handler = async (req: Request, res: Response): Promise<void> => {
+
+  const { name, description } = req.body;
+
+    const { nanoid } = await import("nanoid");
+
+
+  const newTask = {
+    name,
+    description,
+    id: nanoid(),
+  };
+
+  res.json(newTask);
+};
+
+
--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,24 @@
 import lowdb from "lowdb";
 import FileSync from "lowdb/adapters/FileSync";
 
+export type Task = {
+ id: string;
+ name: string;
+ description: string;
+};
 
+type Schema = {
+ tasks: Task[];
+};
+
+let db: lowdb.LowdbSync<Schema>;
 
 export const createConnection = () => {
-  const adapter = new FileSync('db.json');
-  const db = lowdb(adapter);
+  const adapter = new FileSync<Schema>('db.json');
+  db = lowdb(adapter);
 
   db.defaults({ tasks: [] }).write();
 };
+
+export const getConnection = () => db;
+
